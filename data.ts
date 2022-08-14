b2310dee6220056eb0bd492e07ff1dce07317a3a
ellisvelandia <eyis619@gmail.com>
2022-08-13 22:14:35 -0500

    Final

--- data.ts
+++ data.ts
@@ -107,18 +107,18 @@ export const project: Iproject[] = [
     name: "COVID Tracker",
     description:
       "This app shows a statistical view about corona virus over the world",
-    image_path: "/images/covid.jpg",
-    deployed_url: "https://covid-19-tracker-by-sumit.web.app/",
-    github_url: "https://github.com/Dey-Sumit/covid-19-tracker",
+    image_path: "/images/coming-soon.png",
+    deployed_url: "#",
+    github_url: "https://github.com/Ellisvelandia",
     category: ["react"],
     key_techs: ["React", "Chart.js", "Material UI"],
   },
   {
     id: 2,
     name: "Algorithm Visualizer",
-    image_path: "/images/algoVisual.png",
-    deployed_url: "https://visual-algorithm.web.app/",
-    github_url: "https://github.com/Dey-Sumit/algorithm-visualizer",
+    image_path: "/images/coming-soon.png",
+    deployed_url: "#",
+    github_url: "https://github.com/Ellisvelandia",
     category: ["react"],
     description:
       "An web app which shows how an algorithm (path finding or sorting) works with cool animation",
@@ -128,9 +128,9 @@ export const project: Iproject[] = [
   {
     id: 3,
     name: "Dev Talks",
-    image_path: "/images/dev.jpg",
-    deployed_url: "https://dev-talks.herokuapp.com/",
-    github_url: "https://github.com/Dey-Sumit/Dev-talks",
+    image_path: "/images/coming-soon.png",
+    deployed_url: "#",
+    github_url: "https://github.com/Ellisvelandia",
     category: ["node", "mongo", "react"],
     description:
       "Social Media app for developers who can share project,create posts,etc...",
@@ -148,9 +148,9 @@ export const project: Iproject[] = [
   {
     id: 4,
     name: "Realtime Chat App",
-    image_path: "/images/chatapp.jpg",
-    deployed_url: "https://sumit-chat.netlify.app/",
-    github_url: "https://github.com/Dey-Sumit/chat-app-socket.io-react-node",
+    image_path: "/images/coming-soon.png",
+    deployed_url: "#",
+    github_url: "https://github.com/Ellisvelandia",
     category: ["node", "react"],
     description:
       "Basic Realtime Chat App where one can create a room can talk to each other",
@@ -160,9 +160,9 @@ export const project: Iproject[] = [
   {
     id: 5,
     name: "Tweeter Clone",
-    image_path: "/images/tweetme.jpg",
-    deployed_url: "http://sumaxtweetme.pythonanywhere.com/",
-    github_url: "https://github.com/Dey-Sumit/tweetme",
+    image_path: "/images/coming-soon.png",
+    deployed_url: "#",
+    github_url: "https://github.com/Ellisvelandia",
     category: ["django", "react"],
     description:
       "First Django Project :) | Typical Social Media App where one can post,like ,comment etc",
@@ -172,9 +172,9 @@ export const project: Iproject[] = [
   {
     id: 6,
     name: "Color Classification using tf.js",
-    image_path: "/images/color.jpg",
+    image_path: "/images/coming-soon.png",
     deployed_url: "!#",
-    github_url: "https://github.com/Dey-Sumit/color-classification",
+    github_url: "https://github.com/Ellisvelandia",
     category: ["express"],
     description:
       "Tried ML with JS :) | this app classifies a color using CNN algorithm in browser",
@@ -183,9 +183,9 @@ export const project: Iproject[] = [
   {
     id: 7,
     name: "YouTube using YouTube ",
-    image_path: "/images/youtubeClone.png",
-    deployed_url: "https://not-utube.web.app/",
-    github_url: "https://github.com/Dey-Sumit/youtube-clone-tutorial-up",
+    image_path: "/images/coming-soon.png",
+    deployed_url: "https://github.com/Ellisvelandia",
+    github_url: "https://github.com/Ellisvelandia",
     category: ["express"],
     description:
       'Full(almost) Functional YouTube replica where one can login with his/her youtube account to enjoy "not-YouTube".User can like a video,comment on a video & Much More ',
@@ -201,12 +201,12 @@ export const project: Iproject[] = [
   {
     id: 8,
     name: "Football App",
-    image_path: "/images/football.png",
-    deployed_url: "https://o-my-goal.web.app/",
-    github_url: "https://github.com/Dey-Sumit/football-app",
+    image_path: "/images/coming-soon.png",
+    deployed_url: "#",
+    github_url: "https://github.com/Ellisvelandia",
     category: ["react"],
     description:
-      "o my goal replica where an user can keep an eye on his favorite club.This app will keep providing \n all the statistics of that club.all the fans can also chat ",
+      " my goal replica where an user can keep an eye on his favorite club.This app will keep providing \n all the statistics of that club.all the fans can also chat ",
     key_techs: ["React", "Redux", "Firebase Auth", "API", "Sass", "Bootstrap"],
   },
 ];
